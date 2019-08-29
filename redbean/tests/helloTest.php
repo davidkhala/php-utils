986c02a98fdf34ae100e47b7516ea8b395dcc98c
davidLiu <david-khala@hotmail.com>
2019-08-29 14:31:11 +0800

    use default `sys` database

--- redbean/tests/helloTest.php
+++ redbean/tests/helloTest.php
@@ -1,11 +1,14 @@
 <?php
-require_once 'vendor/autoload.php';
+require_once '../vendor/autoload.php';
 
 use \RedBeanPHP\R as R;
-// schema=test should exist in advance
-R::setup( "mysql:host=localhost;dbname=test", 'root' ,'password');
 
-$book = R::dispense("book");
-$book->author = "Santa Claus";
-$book->title = "Secrets of Christmas";
-$id = R::store( $book );
+// schema=test should exist in advance
+$dbname = 'sys';
+R::setup("mysql:host=localhost;dbname=$dbname", 'root', 'password');
+$result = R::getDatabaseAdapter()->getDatabase()->GetAll('show tables;');
+print_r($result);
+//$book = R::dispense("book");
+//$book->author = "Santa Claus";
+//$book->title = "Secrets of Christmas";
+//$id = R::store( $book );
