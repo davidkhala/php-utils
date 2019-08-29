46cf25e34844c09291f844e54a6e0314f80841f8
david <david-khala@hotmail.com>
2019-08-29 11:08:39 +0800

    test done: we need php connect installed

--- redbean/hello.php
+++ redbean/hello.php
@@ -1,11 +1,10 @@
 <?php
-echo "hello world";
 require_once 'vendor/autoload.php';
 
 use \RedBeanPHP\R as R;
-R::setup( "mysql:dbname=dbname", 'root' );
+R::setup( "mysql:host=localhost;dbname=test", 'root' ,'password');
 
 $book = R::dispense("book");
-//$book->author = "Santa Claus";
-//$book->title = "Secrets of Christmas";
-//$id = R::store( $book );
+$book->author = "Santa Claus";
+$book->title = "Secrets of Christmas";
+$id = R::store( $book );
